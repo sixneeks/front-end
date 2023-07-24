2001d0acdaef46c5351f61473c3e6379c42a4575
2Zerozero <hawgae@gmail.com>
2023-07-24 15:25:00 +0900

    Design : 세부 페이지 구현

--- src/pages/Detail/Detail.jsx
+++ src/pages/Detail/Detail.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import Header from '../../components/Header'
+import CardInner from '../../components/CardInner'
+import Footer from '../../components/Footer'
+import { styled } from 'styled-components'
+
+
 
 function Detail() {
   return (
-    <div></div>
+    <Wrap>
+      <Header />
+      <CardInner />
+      <Footer />
+    </Wrap>
   )
 }
 
 export default Detail
+
+const Wrap = styled.div`
+
+`
