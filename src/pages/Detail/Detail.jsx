8541b0243986acb3b85f64579ee3df0ea313f673
2Zerozero <hawgae@gmail.com>
2023-07-25 21:49:31 +0900

    feat : 다크모드 현재 텍스트 색상만 변경됨

--- src/pages/Detail/Detail.jsx
+++ src/pages/Detail/Detail.jsx
@@ -3,21 +3,25 @@ import Header from '../../components/Header'
 import CardInner from '../../components/CardInner'
 import Footer from '../../components/Footer'
 import { styled } from 'styled-components'
-
-
+import ThemeToggle from '../../theme/ThemeToggle'
+import { ThemeProvider } from '../../hooks/ThemeProvider';
+import { GlobalStyle } from '../../theme/GlobalStyles';
 
 function Detail() {
   return (
-    <Wrap>
-      <Header />
-      <CardInner />
-      <Footer />
-    </Wrap>
+    <ThemeProvider>
+      <GlobalStyle />
+      <Wrap>
+        <Header />
+        <CardInner />
+        <Footer />
+        <ThemeToggle />
+      </Wrap>
+    </ThemeProvider>
   )
 }
 
 export default Detail
 
 const Wrap = styled.div`
-
 `
