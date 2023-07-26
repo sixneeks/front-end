c57b15a54cae307a89ad50892b53015575ab4809
2Zerozero <hawgae@gmail.com>
2023-07-26 10:38:27 +0900

    feat : 상세 페이지 api 연결

--- src/pages/Detail/Detail.jsx
+++ src/pages/Detail/Detail.jsx
@@ -6,14 +6,26 @@ import { styled } from 'styled-components'
 import ThemeToggle from '../../theme/ThemeToggle'
 import { ThemeProvider } from '../../hooks/ThemeProvider';
 import { GlobalStyle } from '../../theme/GlobalStyles';
+import { useQuery } from 'react-query'
+import { useParams } from 'react-router-dom'
+import { getDetailPosts } from '../../axios/api'
 
 function Detail() {
+  const { id } = useParams();
+  const { data: detailData, isLoading, isError } = useQuery(['detailPosts', id], () => getDetailPosts(id));
+
   return (
     <ThemeProvider>
       <GlobalStyle />
       <Wrap>
         <Header />
-        <CardInner />
+        {isLoading ? (
+          <div>Loading...</div>
+        ) : isError ? (
+          <div>데이터를 가져오는 중 오류가 발생했습니다.</div>
+        ) : (
+          detailData && <CardInner data={detailData} />
+        )}
         <Footer />
         <ThemeToggle />
       </Wrap>
